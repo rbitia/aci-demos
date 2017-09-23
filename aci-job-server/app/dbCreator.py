77f180749abd4f9091ea4657100d3527ddcda2e2
Sam Kreter <samkreter@gmail.com>
2017-09-23 16:06:15 -0700

    added timing to the database structure

--- aci-job-server/app/dbCreator.py
+++ aci-job-server/app/dbCreator.py
@@ -10,3 +10,18 @@ for root, dirs, files in os.walk('./Pics'):
         VALUES (\"" + filename + "\");")
 
 conn.commit()
+
+# CREATE TABLE jobs (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     filename NOT NULL,
+#     processed INTEGER DEFAULT 0 NOT NULL,
+#     detected INTEGER DEFAULT NULL
+# );
+
+# CREATE TABLE time (
+#     id INTEGER PRIMARY KEY,
+#     start_time TEXT,
+#     started INTEGER
+# );
+
+# INSERT INTO time values(1,CURRENT_TIME,0);
--- aci-job-server/app/run.py
+++ aci-job-server/app/run.py
@@ -6,16 +6,28 @@ from flask import request
 import json
 import sqlite3
 import requests
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    print("yo")
     conn = sqlite3.connect('jobs.db')
+
     cursor = conn.execute("SELECT * FROM jobs WHERE processed = 0 ORDER BY RANDOM() LIMIT 1")
 
+    print("hello1")
     row = cursor.fetchone()
+
+    cursor = conn.execute("SELECT * FROM time WHERE id = 1;")
+    print("hello")
+
+    if(cursor.fetchone()[2] == 0):
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        conn.execute("UPDATE time set started = 1, start_time = \""+ current_time +"\" where id = 1;")
+        conn.commit()
 
     id = row[0]
     filename = row[1]
@@ -49,41 +61,39 @@ def processed():
 
 @app.route('/getProgress')
 def getProgress():
+    current_time = datetime.now()
+
     conn = sqlite3.connect('jobs.db')
     cursor = conn.execute("SELECT * FROM jobs WHERE detected IS NOT NULL;")
+    cursor2 = conn.execute("SELECT * FROM time WHERE id = 1;")
 
-    info = []
+
+    start_time = datetime.strptime(cursor2.fetchone()[1],'%Y-%m-%d %H:%M:%S')
+    total_time = (current_time - start_time).total_seconds()
+
+    print(total_time)
+
+    pictures = []
 
     for row in cursor:
         obj = {
             "filename": row[1],
             "detected": row[3]
         }
-        info.append(obj)
+        pictures.append(obj)
 
-    data = json.dumps(info)
+    data = {
+        "pictures": pictures,
+        "total_time": total_time
+    }
 
-    return  request.args.get('callback') + "(" +  data + ")"
+    json_data = json.dumps(data)
 
-
-
-
+    return  request.args.get('callback') + "(" +  json_data + ")"
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000)
+    app.run(debug=True,host='0.0.0.0',port=8000)
 
-
-#Going fast so just thowing this here for now
-# '''
-# CREATE TABLE jobs (
-#     id INTEGER PRIMARY KEY AUTOINCREMENT,
-#     filename NOT NULL,
-#     processed INTEGER DEFAULT 0 NOT NULL,
-#     detected INTEGER DEFAULT NULL
-# );
-
-# insert into jobs(filename) values("hello");
-# '''
 
