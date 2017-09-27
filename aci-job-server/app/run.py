89925579b0dfe82eada37b181b03fe389ebb80b6
Ria Bhatia <riabhatia@Rias-MacBook-Pro.local>
2017-09-27 06:34:33 -0700

    hello

--- aci-job-server/app/run.py
+++ aci-job-server/app/run.py
@@ -9,26 +9,36 @@ import requests
 from datetime import datetime
 import os
 
+
+import logging
+from logging.handlers import RotatingFileHandler
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-
+    if not os.path.isfile('jobs.db'):
+        exit("There must be a database, try running dbCreator")
+    
     conn = sqlite3.connect('jobs.db')
 
-    cursor = conn.execute("SELECT * FROM jobs WHERE processed = 0 ORDER BY RANDOM() LIMIT 1")
+    row = conn.execute("SELECT * FROM jobs WHERE processed = 0 ORDER BY RANDOM() LIMIT 1").fetchone()
 
-    row = cursor.fetchone()
 
-    if(row == None):
+    if(row == None ):
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        conn.execute("UPDATE time set finished = 1, finish_time = \""+ current_time +"\" where id = 1;")
+        conn.commit()
+
         return json.dumps({
             'filename':"NULL",
-            'processed':1
-            })
+            'processed':1,
+        })
 
     cursor = conn.execute("SELECT * FROM time WHERE id = 1;")
 
-    if(cursor.fetchone()[2] == 0):
+    if(cursor.fetchone()[4] == 0):
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         conn.execute("UPDATE time set started = 1, start_time = \""+ current_time +"\" where id = 1;")
         conn.commit()
@@ -59,15 +69,29 @@ def processed():
     conn.close()
     return "all good"
 
+@app.route('/resetDb')
+def resetDb():
+    conn = sqlite3.connect('jobs.db')
+    conn.execute("UPDATE jobs set detected = NULL, processed = 0;" )
+    conn.execute("UPDATE time set started = 0, finished = 0;" )
+    conn.commit()
+
+    return "The database has been reset, no worries, hope you ment to do that, theres no going back man"
+
 @app.route('/getProgress')
 def getProgress():
     current_time = datetime.now()
 
     conn = sqlite3.connect('jobs.db')
     cursor = conn.execute("SELECT * FROM jobs WHERE detected IS NOT NULL;")
-    cursor2 = conn.execute("SELECT * FROM time WHERE id = 1;")
+    time_data = conn.execute("SELECT * FROM time WHERE id = 1;").fetchone()
 
-    start_time = datetime.strptime(cursor2.fetchone()[1],'%Y-%m-%d %H:%M:%S')
+    start_time = datetime.strptime(time_data[1],'%Y-%m-%d %H:%M:%S')
+
+    if(time_data[3] == 1):
+        current_time = datetime.strptime(time_data[2],'%Y-%m-%d %H:%M:%S')
+
+
     total_time = (current_time - start_time).total_seconds()
 
     pictures = []
@@ -88,41 +112,9 @@ def getProgress():
 
     return  request.args.get('callback') + "(" +  json_data + ")"
 
-def setupDatabase():
-    conn = sqlite3.connect('jobs.db')
 
-    conn.execute('''DROP TABLE IF EXISTS jobs;''')
-    conn.execute('''
-        CREATE TABLE jobs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            filename NOT NULL,
-            processed INTEGER DEFAULT 0 NOT NULL,
-            detected INTEGER DEFAULT NULL
-        );
-        ''')
-
-    conn.execute('DROP TABLE IF EXISTS time;')
-    conn.execute('''
-        CREATE TABLE time (
-        id INTEGER PRIMARY KEY,
-        start_time TEXT,
-        started INTEGER
-    );
-    ''')
-
-    conn.execute('INSERT INTO time values(1,"2017-09-23 18:28:24",0);')
-
-    for root, dirs, files in os.walk('./Pics'):
-        for filename in files:
-            if(filename[:2] == "._"):
-                filename = filename[2:]
-            conn.execute("INSERT INTO jobs (filename) \
-            VALUES (\"" + filename + "\");")
-
-    conn.commit()
 
 if __name__ == '__main__':
-    setupDatabase()
     app.run(debug=True,host='0.0.0.0',port=80)
 
 
--- demo-aci-image/app/run.py
+++ demo-aci-image/app/run.py
@@ -18,4 +18,4 @@ def chart3d():
     return render_template('charts.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=80)
+    app.run(debug=True, host='0.0.0.0',port=8000)
