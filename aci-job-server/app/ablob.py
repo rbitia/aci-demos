d6bee96690bf741a0e7410c67684ccd2512971e7
sakreter <sakreter@microsoft.com>
2017-10-18 09:53:37 -0700

    adding blob storage to database generation

---
+++ aci-job-server/app/ablob.py
@@ -0,0 +1,56 @@
+import os
+from azure.storage.blob import BlockBlobService
+import sqlite3
+
+
+COPY_PICS_NUM = 10
+AZURE_BLOB_ACCOUNT = os.environ.get('AZURE_BLOB_ACCOUNT')
+AZURE_BLOB_KEY = os.environ.get('AZURE_BLOB_KEY')
+
+AZURE_BLOB_KEY = False
+
+if not AZURE_BLOB_ACCOUNT or not AZURE_BLOB_KEY:
+    raise EnvironmentError("Must have env variables AZURE_BLOB_ACCOUNT and AZURE_BLOB_KEY set for this to work.")
+
+block_blob_service = BlockBlobService(account_name= AZURE_BLOB_ACCOUNT, account_key=AZURE_BLOB_KEY)
+
+#block_blob_service.get_blob_to_path('pictures', 'UNADJUSTEDNONRAW_thumb_ff.jpg', 'UNADJUSTEDNONRAW_thumb_ff.jpg')
+
+def setupDatabase():
+    conn = sqlite3.connect('jobs.db')
+
+    conn.execute('''DROP TABLE IF EXISTS jobs;''')
+    conn.execute('''
+        CREATE TABLE jobs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            filename NOT NULL,
+            processed INTEGER DEFAULT 0 NOT NULL,
+            detected INTEGER DEFAULT NULL
+        );
+        ''')
+
+    conn.execute('DROP TABLE IF EXISTS time;')
+    conn.execute('''
+        CREATE TABLE time (
+        id INTEGER PRIMARY KEY,
+        start_time TEXT,
+        finish_time TEXT,
+        finished INTEGER,
+        started INTEGER
+    );
+    ''')
+
+    conn.execute('INSERT INTO time values(1,"2017-09-23 18:28:24","2017-09-23 18:28:24",0,0);')
+
+    generator = block_blob_service.list_blobs('pictures')
+    for blob in generator:
+        if(blob.name[:2] == "._"):
+            blob.name = blob.name[2:]
+        for i in range(COPY_PICS_NUM):
+            conn.execute("INSERT INTO jobs (filename) \
+                VALUES (\"" + blob.name + "\");")
+
+    conn.commit()
+
+if __name__ == '__main__':
+    setupDatabase()
